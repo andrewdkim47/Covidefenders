dd382827598424e6567e2293068ed7d0df5dce1c
jaeyoony <jaeyoony@umich.edu>
2020-04-05 16:04:13 -0400

    added API key for charitysearch

--- covidefenders.py
+++ covidefenders.py
@@ -77,9 +77,22 @@ def setUpCountries(countries, cur, conn):
 
 # ---------------- CHARITY API FUNCTIONS -----------------
 
+CHARITY_URL = 'http://data.orghunter.com/v1/charitysearch'
+CACHE_CHARITY = dir_path + '/' + 'cache_countries.json'
+CHARITY_KEY = '1e879751a28c64cdf4b24a3a5137ef6f'
+
 # TODO: create a cache file for charity api stuff
 def create_charities():
-    pass
+    try: 
+        req = requests.get(CHARITY_URL)
+        char_dict = json.loads(req.text)
+
+    except: 
+        print("ERROR: Trouble fetching charity information")
+        return None
+
+    chairty_data = []
+    for 
 
 # TODO:
 def setUpCharities(countries, cur, conn):
